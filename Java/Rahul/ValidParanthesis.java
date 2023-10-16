0080bf0466be82a7865b13ee9890ed79676c72fe
RahulKakarla <kakarla356@gmail.com>
2023-10-16 16:18:53 -0500

    Initial code
    will try to run and update if there are any issues

--- Java/Rahul/ValidParanthesis.java
+++ Java/Rahul/ValidParanthesis.java
@@ -28,3 +28,44 @@
   1 <= s.length <= 104
   s consists of parentheses only '()[]{}'.
 */
+
+//first checking if a string is valid or not
+
+ public static void main(String[] args) {
+  
+    String x = "()";
+	int len = x.length();
+	if(!x.matches("[()\\[\\]{}]+") || (x.length()<1 || x.length()>104) || x.length() %2 	!= 0){
+    	System.out.println("InValid");
+	}
+    String inChar = "[{(";
+	StringBuffer inptSide = new StringBuffer((len/2)+1); 
+	for (int i=0 ;i< len ;i++){
+    	char right = x.charAt(i);
+    	if(inChar.contains(String.valueOf(right))){
+            inptSide.append(right);
+        }
+        else{
+        	if(inptSide.length()<1){
+            	System.out.println("InValid");
+            }
+            char lft = inptSide.charAt(inptSide.length()-1);
+            System.out.println("right "+right);
+            System.out.println("left "+lft);
+
+            if((right == ')' && lft == '(') || (right == ']' && lft == '[') || (right == '}' && lft == '{')){
+				System.out.println(inptSide.charAt(inptSide.length()-1));
+                //inptSide = inptSide.deleteCharAt(inptSide.length()-1);
+            }
+            	
+        	System.out.println(inptSide.charAt(inptSide.length()-1));
+        }
+	}
+    if(inptSide.length()>0){
+    	System.out.println("InValid");
+    }else{
+    	System.out.println("Valid");
+    }
+  }
+
+
