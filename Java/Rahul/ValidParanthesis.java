1d22139e0a36fdfd595e73bddfb78d7499577b7e
RahulKakarla <kakarla356@gmail.com>
2023-10-16 16:45:49 -0500

    Update ValidParanthesis.java

--- Java/Rahul/ValidParanthesis.java
+++ Java/Rahul/ValidParanthesis.java
@@ -31,9 +31,10 @@
 
 //first checking if a string is valid or not
 
- public static void main(String[] args) {
+ public class Main {
+  public static void main(String[] args) {
   
-    String x = "()";
+    String x = "()[]{}";
 	int len = x.length();
 	if(!x.matches("[()\\[\\]{}]+") || (x.length()<1 || x.length()>104) || x.length() %2 	!= 0){
     	System.out.println("InValid");
@@ -48,17 +49,14 @@
         else{
         	if(inptSide.length()<1){
             	System.out.println("InValid");
+                return;
             }
             char lft = inptSide.charAt(inptSide.length()-1);
-            System.out.println("right "+right);
-            System.out.println("left "+lft);
 
             if((right == ')' && lft == '(') || (right == ']' && lft == '[') || (right == '}' && lft == '{')){
-				System.out.println(inptSide.charAt(inptSide.length()-1));
-                //inptSide = inptSide.deleteCharAt(inptSide.length()-1);
+                inptSide = inptSide.deleteCharAt(inptSide.length()-1);
             }
             	
-        	System.out.println(inptSide.charAt(inptSide.length()-1));
         }
 	}
     if(inptSide.length()>0){
@@ -67,5 +65,4 @@
     	System.out.println("Valid");
     }
   }
-
-
+}
