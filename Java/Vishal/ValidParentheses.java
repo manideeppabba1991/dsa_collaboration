96ea2986e67e6ece00d7094e09cb43a469a58b7f
manideeppabba1991 <manideeppabba@gmail.com>
2023-10-17 16:25:48 -0500

    Update ValidParentheses.java

--- Java/Vishal/ValidParentheses.java
+++ Java/Vishal/ValidParentheses.java
@@ -28,3 +28,43 @@
   1 <= s.length <= 104
   s consists of parentheses only '()[]{}'.
 */
+
+Boolean SampleProblem(String input) {
+        int numberOfOpenRoundParenthesis = 0;
+        int numberOfClosedRoundParenthesis = 0;
+        int numberOfOpenFlowerParenthesis = 0;
+        int numberOfClosedFlowerParenthesis = 0;
+        int numberOfOpenSquareParenthesis = 0;
+        int numberOfClosedSquareParenthesis = 0;
+        boolean outPut=true;
+        int counter = 0;
+        if(input != null) {
+            for(int i=0; i<input.length(); i++) {
+                if("(".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfOpenRoundParenthesis++;
+                }
+                if(")".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfClosedRoundParenthesis++;
+                }
+                if("[".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfOpenSquareParenthesis++;
+                }
+                if("]".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfClosedSquareParenthesis++;
+                }
+                if("{".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfOpenFlowerParenthesis++;
+                }
+                if("}".equalsIgnoreCase(String.valueOf(input.charAt(i)))) {
+                    numberOfClosedFlowerParenthesis++;
+                }
+                counter++;
+            }
+            if(numberOfOpenRoundParenthesis!=numberOfClosedRoundParenthesis
+                    || numberOfOpenFlowerParenthesis!=numberOfClosedFlowerParenthesis
+                    || numberOfOpenSquareParenthesis!=numberOfClosedSquareParenthesis) {
+                outPut=false;
+            }
+        }
+        return outPut;
+    }
