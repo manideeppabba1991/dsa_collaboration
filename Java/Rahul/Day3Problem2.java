460b62b371d06819a2aa58e4abdb8a7960390a62
RahulKakarla <kakarla356@gmail.com>
2023-10-20 21:26:48 -0500

    Update Day3Problem2.java

--- Java/Rahul/Day3Problem2.java
+++ Java/Rahul/Day3Problem2.java
@@ -1,52 +1,34 @@
+/*
+  time complexity = n or n^2, i believe it is n, but i doubt due to recursion inside for
+  space = n;
+  first i didn't get an idea to use object, I spent more time on setting the input to int [] and unable to figure that out, 
+  later i tried to get some additional help and got the idea of using object, and then was able to get it going.
+*/
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /* Online Java Compiler and Editor */
-public class ProductRecursionArray{
+public class FlatObject{
 
      public static void main(String []args){
-         int[] input1 = new int[]{1,3,5};
+         Object[] input = {1, 2, 3, new Object[]{4, new Object[]{5, new Object[]{6}}}, 7};
          
          List<Integer> out = flatObject(input);
-         for(Integer i: out){
-             System.out.println("out array"+out[i]);
-         }
-
-          
+         System.out.println(out);
      }
      
-     public static List<Integer> flatObject (Object input){
+     public static List<Integer> flatObject (Object[] input){
         List<Integer> output = new ArrayList<Integer>();
-         for(int i=0;i< input.length();i++){
-             if( input[i] instanceof Integer) {
-                 output.add(input[i]);
+         for(Object i: input){
+             if( i instanceof Integer) {
+                 output.add((Integer)i);
              }else{
-                 output.addAll(flatObject(input[i]));
-                 
+                 output.addAll(flatObject((Object[]) i));
              }
          }
          return output;
          
-     }
-     
-     public static Boolean palindromeRecur (String input){
-         System.out.println(" value:"+ input);
-         if(input.length()==1){
-             return true;
-         }
-         if(input.length()==2 && input.charAt(0) == input.charAt(input.length()-1)){
-             return true;
-         }
-         if(input.charAt(0) != input.charAt(input.length()-1)){
-             return false;
-         }
-         
-         return (input.charAt(0) == input.charAt(input.length()-1)) 
-         && palindromeRecur(input.substring(1,input.length()-1));
-
-         
-     }
-     
-   
+     } 
 }
