1da7f0ce7cda33b4c22c09f9a7977387c9d25122
rathnakare <erathnakarreddy@gmail.com>
2023-10-24 14:25:33 -0500

    added more comments
    added more descriptive comments

--- Java/Rathna/ImplementMergeSort.java
+++ Java/Rathna/ImplementMergeSort.java
@@ -12,16 +12,26 @@ public class ImplementMergeSort {
     }
 
     private static int[] mergeSort(int[] arr) {
-        if(arr.length == 1) { //Based condition for recursion
+        if(arr.length == 1) { //Base condition for recursion
             return arr;
         }
         //Follow Divide and Conquer procedure
-        //Divide the array and handle each part individually
+        //Divide the array and handle each part recursively until we reach base condition on each side
         int mid = arr.length/2;
         System.out.println("Value of mid: " + mid);
-        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
-
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid)); //Based on the mid-value, we create a new array of size mid (First call, mid = 3 then 1, 1, 1 for recursive calls)
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length)); //Based on the mid-value, we create a new array of size mid (First call, mid = 3 then 1, 1, 1 for recursive calls)
+        //Now we have individually arrays, which we will sort using two pointers below
+        System.out.print("Left Side: ");
+        for(int i : left) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.print("Right Side: ");
+        for(int i : right) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
         int[] sortedArr = mergeTwoArrays(left, right);
         return sortedArr;
     }
@@ -33,16 +43,16 @@ public class ImplementMergeSort {
         int[] result = new int[left.length + right.length];
 
         while(lef < left.length &&  rig < right.length) { //TC = O(nlog(n)) and SC = O(1)
-            if(left[lef] < right[rig]) {
+            if(left[lef] < right[rig]) { // Compare left and right arrays and lesser values goes into result array
                 result[count++] = left[lef++];
             } else {
                 result[count++] = right[rig++];
             }
         }
-        while(lef < left.length) {
+        while(lef < left.length) { // If any additional elements in left array gets directly added to result array
             result[count++] = left[lef++];
         }
-        while(rig < right.length) {
+        while(rig < right.length) { // If any additional elements in right array gets directly added to result array
             result[count++] = right[rig++];
         }
         return result;
