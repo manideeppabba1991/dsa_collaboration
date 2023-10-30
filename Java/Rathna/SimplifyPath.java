8e29a87f457d56ec4823b241e04695f2b98f4c0f
rathnakare <erathnakarreddy@gmail.com>
2023-10-29 22:31:50 -0500

    missed few scenarios in my first approach, fixed them after research

--- Java/Rathna/SimplifyPath.java
+++ Java/Rathna/SimplifyPath.java
@@ -1,22 +1,28 @@
+import java.util.Stack;
+
 public class SimplifyPath {
     public static void main(String[] args) {
-        String path = "/home//foo//yoo";
+        String path = "/home/..//foo//../../yoo/"; // after splitting = "", home, .., foo, .., .., yoo
         path = pathSimplify(path);
         System.out.println(path);
     }
+
     //Method to simplifyPath based on certain given constraints to return canonical path
     private static String pathSimplify(String path) {
-        String homePath = "/../";
-        String directoryPath = "//";
-        if(path.contains(homePath)) {
-            path = path.replace(homePath, "/");
+        Stack<String> stack = new Stack<>();
+        String[] folderArray = path.split("/+");
+        for (String fold : folderArray) {
+            System.out.println("Contents of folderArray is: " + fold);
+            if (fold.equals("..") && !stack.isEmpty()) {
+                stack.pop();
+            } else if (!(fold.equals("..") || fold.equals(".") || fold.equals(""))) {
+                stack.add(fold);
+            }
         }
-        if(path.contains(directoryPath)) {
-            path = path.replace(directoryPath, "/");
+        String result = "";
+        for (String dir : stack) {
+            result += "/" + dir;
         }
-        if(path.length() > 1 && path.charAt(path.length() - 1) == '/') {
-            path = path.substring(0, path.length() - 1);
-        }
-        return path;
+        return result.isEmpty() ? "/" : result;
     }
 }
